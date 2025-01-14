46789096fe90ace72e15946829144923c3257f50
Yuvan-sP <yuvan2364@gmail.com>
2025-01-15 00:18:11 +0530

    Initial commit for Java project

---
+++ react_frontend/src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class FooterComponent extends Component {
+    render() {
+        return (
+            <div>
+                <footer className='footer'>
+                    <span>
+                        All Right Reserved 2024 &copy; Siddu
+                    </span>
+                </footer>
+            </div>
+        );
+    }
+}
---
+++ springboot1-backend-1/src/main/java/com/ihub/www/Springboot1Backend1Application.java
@@ -0,0 +1,13 @@
+package com.ihub.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1Backend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot1Backend1Application.class, args);
+	}
+
+}
