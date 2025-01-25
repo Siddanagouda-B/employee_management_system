829aa2d5bd928226f3ef4c0f3c71432ce5abdea2
Siddanagoud S B <siddanagoudasbs@gmail.com>
2025-01-26 00:30:35 +0530

    Add Java project files

--- springboot1-backend-1/src/main/java/com/ihub/www/controller/EmployeeController.java
+++ springboot1-backend-1/src/main/java/com/ihub/www/controller/EmployeeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ihub.www.model.Employee;
 import com.ihub.www.service.EmployeeService;
 
-@CrossOrigin(origins = "http://localhost:3003")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
